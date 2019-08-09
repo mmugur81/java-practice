a4f1b85c70311fd2ab186d238528007c87158f9e
Mugurel Mirica <mmugur81@gmail.com>
2019-08-09 14:48:21 +0300

    BinaryTreeToDll - finish

--- src/main/java/practice/BinaryTreeToDll.java
+++ src/main/java/practice/BinaryTreeToDll.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
  * Given a Binary Tree (BT), convert it to a Doubly Linked List(DLL)
  * <p>
  * Test case: 1 4 10 20 L 10 30 R 20 40 L 20 60 R
+ * Test case: 1 14 0 1 L 0 2 R 1 3 L 1 4 R 2 5 L 2 6 R 3 7 L 3 8 R 4 9 L 4 10 R 5 11 L 5 12 R 6 13 L 6 14 R
  */
 public class BinaryTreeToDll {
 
@@ -19,13 +20,23 @@ public class BinaryTreeToDll {
             int nNodes = scanner.nextInt();
             Node node = readTree(nNodes);
 
-            Node dll = convertToDoubleLinkedList(node);
+            Node nodeList = convertToDoubleLinkedList(node);
 
-            while (dll != null) {
-                System.out.print(" " + dll.getData());
-                dll = dll.getRight();
+            // Left view print - and also setting the left link
+            Node lastNode = null;
+            while (nodeList != null) {
+                nodeList.setLeft(lastNode);
+                lastNode = nodeList;
+                System.out.print(" " + nodeList.getData());
+                nodeList = nodeList.getRight();
             }
+            System.out.println("");
 
+            // Right view print
+            while (lastNode != null) {
+                System.out.print(" " + lastNode.getData());
+                lastNode = lastNode.getLeft();
+            }
             System.out.println("\n");
         }
     }
@@ -74,33 +85,49 @@ public class BinaryTreeToDll {
 
         //add the left node
         if (rootNode.getLeft() != null) {
-            leftNode = new Node();
-            leftNode.setData(centralNode.getLeft().getData());
-            leftNode.setRight(centralNode);
-            centralNode.setLeft(leftNode);
-            convertToDoubleLinkedList(rootNode.getLeft());
+
+            // Check left side in depth
+            Node leftTree = convertToDoubleLinkedList(rootNode.getLeft());
+            if (leftTree != null) {
+                // replace leftNode with leftTree
+                leftNode = leftTree;
+
+                //set the correct right link for the last element from leftTree
+                Node lastNode = leftNode;
+                while (lastNode.getRight() != null) {
+                    lastNode = lastNode.getRight();
+                }
+                lastNode.setRight(centralNode);
+
+            } else {
+                // leftNode does not have more levels on left side, so it's alone
+                leftNode = new Node();
+                leftNode.setData(rootNode.getLeft().getData());
+                leftNode.setRight(centralNode);
+            }
+            //left side will be set after the first traversal from left to right
         }
 
         //add the right node
         if (rootNode.getRight() != null) {
-            rightNode = new Node();
-            rightNode.setData(rootNode.getRight().getData());
-            rightNode.setLeft(rootNode);
+
+            // check right side in depth
+            Node rightTree = convertToDoubleLinkedList(rootNode.getRight());
+            if (rightTree != null) {
+                //replace rightNode with rightTree
+                rightNode = rightTree;
+            } else {
+                // rightNode does not have more levels on right side, so it's alone
+                rightNode = new Node();
+                rightNode.setData(rootNode.getRight().getData());
+
+            }
+            //left side will be set after the first traversal from left to right
             centralNode.setRight(rightNode);
-            convertToDoubleLinkedList(rootNode.getRight());
         }
 
         //return the leftmost node
         return leftNode;
-    }
-
-    public static Node getOuterLeft(Node tree) {
-        Node node = tree;
-        while (node.getLeft() != null) {
-            node = node.getLeft();
-        }
-
-        return node;
     }
 
     public static class Node {
